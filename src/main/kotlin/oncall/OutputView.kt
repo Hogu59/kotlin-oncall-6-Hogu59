422c3249e9f6d2475bd8ad9c856a00e1c9bf785b
Hogu59 <dbstjdguswkd@naver.com>
2023-12-16 17:18:01 +0900

    FEAT: Implement Print Duty List

--- src/main/kotlin/oncall/OutputView.kt
+++ src/main/kotlin/oncall/OutputView.kt
@@ -1,9 +1,16 @@
 package oncall
 
 class OutputView {
-    fun printOnCallMonthDayGuideMention() = println(ON_CALL_MONTH_DAY_INPUT_MENTION)
+    fun printWorkingList(date: Date, dutyRoster: List<String>) {
+        val month = date.month
+        val day = date.day
+        for (i in 1..getMaxDateOfMonth(month)) {
+            println("${month}월 ${i}일 ${date.getDayOfTheDate(day, i)}${getHolidayMark(date, i)} ${dutyRoster[i-1]}")
+        }
+    }
 
-    companion object {
-        const val ON_CALL_MONTH_DAY_INPUT_MENTION = "비상 근무를 배정할 월과 시작 요일을 입력하세요>"
+    fun getHolidayMark(date: Date, dateNum: Int): String {
+        if(date.getDayOfTheDate(date.day, dateNum) != "토" && date.getDayOfTheDate(date.day, dateNum) != "일" && date.checkIsHoliday(dateNum)) return "(휴일)"
+        return ""
     }
 }
